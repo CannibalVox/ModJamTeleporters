efb44f486518c7ab5b429a0ab304cade5e1f5995
CannibalVox <sdbaynham@gmail.com>
2014-05-14 19:40:28 -0500

    AutoCommit: 2014-05-14 19:40:27.975099

--- src/main/java/net/modyssey/teleporters/ModysseyTeleporters.java
+++ src/main/java/net/modyssey/teleporters/ModysseyTeleporters.java
@@ -1,5 +1,17 @@
 package net.modyssey.teleporters;
 
-@Mod()
+import cpw.mods.fml.common.Mod;
+import net.minecraft.block.Block;
+
+@Mod(modid = "modysseyteleporters", version = ModysseyTeleporters.VERSION)
 public class ModysseyTeleporters {
+    public static final String VERSION = "1.0";
+
+    public static Block teleporterPad;
+    public static Block teleporterController;
+    public static Block beacon;
+
+    public void preInit() {
+
+    }
 }
