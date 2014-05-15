b04879d925725253e8db3cb9a5d2e901552d5334
CannibalVox <sdbaynham@gmail.com>
2014-05-14 21:47:14 -0500

    AutoCommit: 2014-05-14 21:47:14.086498

--- src/main/java/net/modyssey/teleporters/client/gui/GuiTeleporterController.java
+++ src/main/java/net/modyssey/teleporters/client/gui/GuiTeleporterController.java
@@ -1,8 +1,10 @@
 package net.modyssey.teleporters.client.gui;
 
 import net.minecraft.client.gui.inventory.GuiContainer;
+import net.minecraft.util.ResourceLocation;
 import net.modyssey.teleporters.tileentities.TileEntityTeleporterController;
 import net.modyssey.teleporters.tileentities.container.ContainerTeleporterController;
+import org.lwjgl.opengl.GL11;
 
 public class GuiTeleporterController extends GuiContainer {
     private TileEntityTeleporterController controller;
@@ -15,6 +17,10 @@ public class GuiTeleporterController extends GuiContainer {
 
     @Override
     protected void drawGuiContainerBackgroundLayer(float var1, int var2, int var3) {
-
+        GL11.glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
+        mc.renderEngine.bindTexture(new ResourceLocation("modysseyteleporters:gui/StarMallBack.png"));
+        int l = (width - xSize) / 2;
+        int i1 = (height - ySize) / 2;
+        drawTexturedModalRect(l, i1, 0, 0, xSize, ySize);
     }
 }
