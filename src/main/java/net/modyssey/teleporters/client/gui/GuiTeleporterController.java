05a4a52ea4053be79c742c2796a229db3b3a980f
CannibalVox <sdbaynham@gmail.com>
2014-05-16 05:44:02 -0500

    AutoCommit: 2014-05-16 05:44:02.180046

--- src/main/java/net/modyssey/teleporters/client/gui/GuiTeleporterController.java
+++ src/main/java/net/modyssey/teleporters/client/gui/GuiTeleporterController.java
@@ -11,6 +11,11 @@ public class GuiTeleporterController extends GuiContainer {
     private TileEntityTeleporterController controller;
 
     private int marketIndex = 0;
+    private static final int MARKET_COUNT = 2;
+    private static final String[] MARKET_ICONS = {
+            "modysseyteleporters:textures/gui/station_title_starmall.png",
+            "modysseyteleporters:textures/gui/station_title_starmall.png"
+    };
 
     public GuiTeleporterController(TileEntityTeleporterController controller) {
         super(new ContainerTeleporterController(controller));
@@ -21,6 +26,10 @@ public class GuiTeleporterController extends GuiContainer {
     @Override
     protected void drawGuiContainerForegroundLayer(int mouseX, int mouseY) {
         drawString(fontRendererObj, "Pads Registered: " + Integer.toString(controller.getPadCount()), 8, 4, 0xFFFFFF);
+    }
+
+    protected void mouseClicked(int par1, int par2, int par3) {
+        super.mouseClicked(par1, par2, par3);
     }
 
     @Override
@@ -45,13 +54,26 @@ public class GuiTeleporterController extends GuiContainer {
         tessellator.addVertexWithUV(x + w, y, (double)this.zLevel, uvW * f, 0);
         tessellator.addVertexWithUV(x, y, (double)this.zLevel, 0, 0);
 
-        tessellator.addVertexWithUV(x - 47.7, y + 70.2, (double)this.zLevel, 195 * f, 52 * f1);
-        tessellator.addVertexWithUV(x, y + 70.2, (double)this.zLevel, 248 * f, 52 * f1);
-        tessellator.addVertexWithUV(x, y + 45, (double)this.zLevel, 248 * f, 24 * f1);
-        tessellator.addVertexWithUV(x - 47.7, y + 45, (double)this.zLevel, 195 * f, 24 * f1);
+        double physicalY = 45;
+        for (int i = 0; i < MARKET_COUNT; i++) {
+            if (i == marketIndex) {
+                tessellator.addVertexWithUV(x - 47.7, y + physicalY + 25.2, (double) this.zLevel, 195 * f, 52 * f1);
+                tessellator.addVertexWithUV(x, y + physicalY + 25.2, (double) this.zLevel, 248 * f, 52 * f1);
+                tessellator.addVertexWithUV(x, y + physicalY, (double) this.zLevel, 248 * f, 24 * f1);
+                tessellator.addVertexWithUV(x - 47.7, y + physicalY, (double) this.zLevel, 195 * f, 24 * f1);
+            } else {
+                tessellator.addVertexWithUV(x - 44.1, y + physicalY + 25.2, (double) this.zLevel, 195 * f, 80 * f1);
+                tessellator.addVertexWithUV(x, y + physicalY + 25.2, (double) this.zLevel, 244 * f, 80 * f1);
+                tessellator.addVertexWithUV(x, y + physicalY, (double) this.zLevel, 244 * f, 52 * f1);
+                tessellator.addVertexWithUV(x - 44.1, y + physicalY, (double) this.zLevel, 195 * f, 52 * f1);
+            }
+
+            physicalY += 25.2;
+        }
+
         tessellator.draw();
 
-        mc.renderEngine.bindTexture(new ResourceLocation("modysseyteleporters:textures/gui/station_title_starmall.png"));
+        mc.renderEngine.bindTexture(new ResourceLocation(MARKET_ICONS[marketIndex]));
         tessellator.startDrawingQuads();
         tessellator.addVertexWithUV(x, y + 30.6, (double)this.zLevel, 0, 1);
         tessellator.addVertexWithUV(x + 118.8, y + 30.6, (double)this.zLevel, 1, 1);
