7c47ad8213f808e00ea494ca9143f857db2a5f98
CannibalVox <sdbaynham@gmail.com>
2014-05-16 03:09:39 -0500

    AutoCommit: 2014-05-16 03:09:39.620650

---
+++ src/main/java/net/modyssey/teleporters/io/PadData.java
@@ -0,0 +1,16 @@
+package net.modyssey.teleporters.io;
+
+import net.minecraft.block.Block;
+import net.minecraft.item.ItemStack;
+
+import java.util.LinkedList;
+
+public class PadData {
+    private int padXCoord;
+    private int padYCoord;
+    private int padZCoord;
+
+    private Block aboveBlock;
+    private LinkedList<ItemStack> blockContents;
+    private LinkedList<ItemStack>
+}
