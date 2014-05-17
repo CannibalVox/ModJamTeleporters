55b04815c440bffe1c7f8d5b40163ad360a2d3f1
CannibalVox <sdbaynham@gmail.com>
2014-05-16 22:13:44 -0500

    AutoCommit: 2014-05-16 22:13:44.021605

--- src/main/java/net/modyssey/teleporters/client/gui/GuiCategoryList.java
+++ src/main/java/net/modyssey/teleporters/client/gui/GuiCategoryList.java
@@ -1,18 +1,16 @@
 package net.modyssey.teleporters.client.gui;
 
-import cpw.mods.fml.client.GuiScrollingList;
-import net.minecraft.client.gui.Gui;
+import net.minecraft.client.gui.GuiListExtended;
 import net.minecraft.client.renderer.Tessellator;
 import net.modyssey.teleporters.markets.stock.StockList;
-import org.lwjgl.input.Mouse;
-import org.lwjgl.opengl.GL11;
 
-public class GuiCategoryList extends GuiScrollingList {
+public class GuiCategoryList extends GuiListExtended {
     private StockList stockList;
     private GuiTeleporterController parent;
 
     public GuiCategoryList(GuiTeleporterController parent, int parentX, int parentY) {
-        super(parent.mc, 55, 112, parentY+53, parentY+164, parentX+9, 20);
+        super(parent.mc, 55, 112, parentY+53, parentY+164, 20);
+        setSlotXBoundsFromLeft(parentX+9);
 
         this.parent = parent;
     }
@@ -22,32 +20,18 @@ public class GuiCategoryList extends GuiScrollingList {
     }
 
     @Override
+    public IGuiListEntry getListEntry(int var1) {
+        return null;
+    }
+
+    @Override
     protected int getSize() {
-        return (stockList == null)?0:stockList.getCategoryCount();
+        return stockList.getCategoryCount();
     }
 
     @Override
-    protected void elementClicked(int index, boolean doubleClick) {
-
-    }
-
-    @Override
-    protected boolean isSelected(int index) {
-        return false;
-    }
-
-    @Override
-    protected void drawBackground() {
-
-    }
-
-    @Override
-    protected void drawGradientRect(int par1, int par2, int par3, int par4, int par5, int par6) {
-
-    }
-
-    @Override
-    protected void drawSlot(int var1, int var2, int var3, int var4, Tessellator var5) {
+    protected void drawContainerBackground(Tessellator tessellator)
+    {
 
     }
 }
