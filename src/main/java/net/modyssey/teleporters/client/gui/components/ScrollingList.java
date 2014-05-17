92f8aa2e33a2eb4cc874b6531073b1c60d577f8c
CannibalVox <sdbaynham@gmail.com>
2014-05-16 23:10:01 -0500

    AutoCommit: 2014-05-16 23:10:01.546373

--- src/main/java/net/modyssey/teleporters/client/gui/components/ScrollingList.java
+++ src/main/java/net/modyssey/teleporters/client/gui/components/ScrollingList.java
@@ -1,5 +1,6 @@
 package net.modyssey.teleporters.client.gui.components;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Gui;
 import net.minecraft.util.ResourceLocation;
 
@@ -27,6 +28,18 @@ public abstract class ScrollingList extends Gui {
         normalizeScrollPosition();
 
         drawScrollbar();
+
+        int y = -viewportTopPosition;
+
+        for (int i = 0; i < getEntryCount(); i++) {
+            int nextY = y + getEntryHeight();
+
+            if (nextY > 0) {
+                drawEntry(i, y);
+            }
+
+            y = nextY;
+        }
     }
 
     protected void normalizeScrollPosition() {
@@ -44,20 +57,45 @@ public abstract class ScrollingList extends Gui {
         double contentSize = getEntryCount() * getEntryHeight();
         double viewportSize = viewportBounds.getHeight();
 
+        //Get the % of the track that the scroll grip makes up (can't be more than 100% of the track)
         double gripToTrackRatio = viewportSize/contentSize;
-
         if (gripToTrackRatio < 1.0)
             gripToTrackRatio = 1;
 
+        //Get the actual grip size in pixels the grip should occupy... shouldn't be smaller than the grip
+        //as shown in the source image
         Rectangle2D trackBounds = getScrollTrackBounds();
         int gripHeight = (int)(trackBounds.getHeight() * gripToTrackRatio);
         Rectangle2D gripBounds = getScrollGripBounds();
 
-        if ((int)gripBounds.getHeight() > gripHeight)
-            gripHeight = (int)gripBounds.getHeight();
+        int gripNativeHeight = (int)gripBounds.getHeight();
+        if (gripNativeHeight > gripHeight)
+            gripHeight = gripNativeHeight;
 
+        //Get the position of the grip on the track
         double slideLength = trackBounds.getHeight() - gripHeight;
+        double contentSlide = contentSize - viewportSize;
+        double slideToContentRatio = slideLength/contentSlide;
 
-        int contentSlide =
+        int gripPosition = (int)(slideToContentRatio * (double)viewportTopPosition);
+
+        int gripNativeWidth = (int)gripBounds.getWidth();
+        int trackX = (int)trackBounds.getX();
+        int trackY = (int)trackBounds.getY();
+        int gripX = (int)gripBounds.getX();
+        int gripY = (int)gripBounds.getY();
+
+        trackY += gripPosition;
+
+        Minecraft.getMinecraft().renderEngine.bindTexture(getScrollGripTexture());
+        while (gripHeight > gripNativeHeight) {
+            drawTexturedModalRect(trackX, trackY, gripX, gripY, gripNativeWidth, gripNativeHeight);
+            trackY += gripNativeHeight;
+            gripHeight -= gripNativeHeight;
+        }
+
+        if (gripHeight != 0) {
+            drawTexturedModalRect(trackX, trackY, gripX, gripY, gripNativeWidth, gripHeight);
+        }
     }
 }
